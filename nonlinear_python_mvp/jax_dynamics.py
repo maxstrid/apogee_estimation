f1d09ba4ce8f15cb30117eb65a8f1856d9369103
Justin Turcotte <jjturcot@gmail.com>
2025-10-18 17:19:08 -0400

    Add simulation with full jax jit
    This speeds up the calculations about an order of magnitude or two faster.

--- nonlinear_python_mvp/jax_dynamics.py
+++ nonlinear_python_mvp/jax_dynamics.py
@@ -2,22 +2,12 @@ from pandas.core.generic import DtypeBackend
 from data_ingest import MassThrustInterpolator
 import jax
 import jax.numpy as jnp
+from scipy.constants import g
 
 # x = [x, v, F_e]
-# u = []
+# u = [thrust, mass]
 # z = [p, a]
 
-g = 9.81
-# RK4 Integration for a function which takes in t and x
-def rk4_tx(f, x, t, dt):
-    h = dt
-
-    k1 = f(t, x)
-    k2 = f(t + (h/2.0), x + h * (k1/2.0))
-    k3 = f(t + (h/2.0), x + h * (k2/2.0))
-    k4 = f(t + h, x + h * k3)
-
-    return x + (h/6.0) * (k1 + 2*k2 + 2*k3 + k4)
 
 class Dynamics:
     def __init__(self, cD, A_ref, rho, mt_interpolator: MassThrustInterpolator) -> None:
@@ -27,10 +17,22 @@ class Dynamics:
 
         self.mt_interpolator = mt_interpolator
 
-    def a(self, x, t) -> float:
-        F_thrust = self.mt_interpolator.thrust(t)
+    # RK4 Integration for a function which takes in t and x
+    def __rk4_tx(self, f, x, t, dt):
+        h = dt
+        U = lambda t: jnp.array([self.mt_interpolator.thrust(t), self.mt_interpolator.mass(t)])
 
-        mass = self.mt_interpolator.mass(t)
+        k1 = f(x, U(t))
+        k2 = f(x + h * (k1/2.0), U(t + (h/2.0)))
+        k3 = f(x + h * (k2/2.0), U(t + (h/2.0)))
+        k4 = f(x + h * k3, U(t+h))
+
+        return x + (h/6.0) * (k1 + 2*k2 + 2*k3 + k4)
+
+    def a(self, x, U) -> float:
+        F_thrust = U[0]
+
+        mass = U[1]
 
         F_weight = mass * g
 
@@ -41,31 +43,24 @@ class Dynamics:
         return (F_thrust - F_drag - F_weight + F_external)/mass
 
     def integrate(self, x0, t0, t1, dt=0.005):
-        n_steps = int(jnp.ceil((t1 - t0) / dt))
-        x = x0
-        trajectory = []
+        n_steps = jnp.ceil((t1 - t0) / dt).astype(int)
 
-        t = t0
-        for _ in range(n_steps):
-            x = rk4_tx(self.f, x, t, dt)
-            trajectory.append(x)
+        def loop(i, carry):
+            t, x0, x_max = carry
+            x = self.__rk4_tx(self.f, x0, t, dt)
             t += dt
+            x_max = jnp.maximum(x[0], x_max)
 
-        return jnp.stack(trajectory)
+            return (t, x, x_max)
 
-    def compute_jacobians(self):
-        self.__A_jit = jax.jit(jax.jacrev(self.f, argnums=1))
-        self.__H_jit = jax.jit(jax.jacrev(self.h, argnums=1))
+        _,_,x_max = jax.lax.fori_loop(lower=0, upper=n_steps, body_fun=loop, init_val=(t0, x0, x0[0]))
 
-    def f(self, t, x) -> float:
-        return jnp.array([x[1], self.a(x, t), 0])
+        return x_max 
 
-    def h(self, t, x) -> float:
-        return jnp.array([x[0], self.a(x, t)])
+    def f(self, x, U):
+        return jnp.array([x[1], self.a(x, U), 0])
 
-    def A(self, t, x):
-        return self.__A_jit(t, x)
+    def h(self, x, U):
+        return jnp.array([x[0], self.a(x, U)])
 
-    def H(self, t, x):
-        return self.__H_jit(t, x)
 
--- nonlinear_python_mvp/replay.py
+++ nonlinear_python_mvp/replay.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from pandas.core.series import series_sub_kwargs
 from data_ingest import FlightDataInterpolator, MassThrustInterpolator
-from ekf import Ekf
+from jaxfilter import ekf
 from jax_dynamics import Dynamics
 import jax.numpy as jnp
+import jax
 import numpy as np
 
 def plot(time, thrust, mass, altitude, velocity, acceleration, altitude_est, velocity_est, acceleration_est, external_force, apogee_estimates):
@@ -14,11 +16,13 @@ def plot(time, thrust, mass, altitude, velocity, acceleration, altitude_est, vel
     axes1[0].set_xlabel('Time (s)')
     axes1[0].set_ylabel('Thrust (N)')
     axes1[0].set_title('Thrust vs Time')
+    axes1[0].legend()
 
     axes1[1].plot(time, mass)
     axes1[1].set_xlabel('Time (s)')
     axes1[1].set_ylabel('Mass (kg)')
     axes1[1].set_title('Mass vs Time')
+    axes1[1].legend()
 
     fig2, axes2 = plt.subplots(5, 1, constrained_layout=True)
 
@@ -28,6 +32,7 @@ def plot(time, thrust, mass, altitude, velocity, acceleration, altitude_est, vel
     axes2[0].set_xlabel('Time (s)')
     axes2[0].set_ylabel('Altitude (m)')
     axes2[0].set_title('Altitude vs Time')
+    axes2[0].legend()
 
     axes2[1].plot(time, velocity, label="Velocity")
     # axes2[1].plot(time, traj_velocity, color='red', linestyle="--", label="Estimated Velocity")
@@ -35,22 +40,26 @@ def plot(time, thrust, mass, altitude, velocity, acceleration, altitude_est, vel
     axes2[1].set_xlabel('Time (s)')
     axes2[1].set_ylabel('Velocity (m/s)')
     axes2[1].set_title('Velocity vs Time')
+    axes2[1].legend()
 
     axes2[2].plot(time, acceleration, color='red', label="Acceleration")
     axes2[2].plot(time, acceleration_est, color='red', linestyle="--", label="Estimated Acceleration")
     axes2[2].set_xlabel('Time (s)')
     axes2[2].set_ylabel('Acceleration (m/s^2)')
     axes2[2].set_title('Acceleration vs Time')
+    axes2[2].legend()
 
     axes2[3].plot(time, external_force, color='red', linestyle="--")
     axes2[3].set_xlabel('Time (s)')
     axes2[3].set_ylabel('External Force (N)')
     axes2[3].set_title('External Force vs Time')
+    axes2[3].legend()
 
     axes2[4].plot(time, apogee_estimates, color='blue', linestyle='--')
     axes2[4].set_xlabel('Time (s)')
     axes2[4].set_ylabel('Apogee Estimate (m)')
     axes2[4].set_title('Apogee Estimate vs Time')
+    axes2[4].legend()
 
     plt.show()
 
@@ -66,16 +75,14 @@ def main():
     acceleration = []
 
     time = []
-    dt = 0.005
 
-    t = 0
 
     cD = 0.6
     A_ref = 0.0049325
     rho = 1.0
 
     dynamics = Dynamics(cD, A_ref, rho, mt_interpolator)
-    dynamics.compute_jacobians()
+    #dynamics.compute_jacobians()
     #x0 = jnp.array([data_interpolator.altitude(0.0), data_interpolator.velocity(0.0), 0.0])  # initial position, velocity, external force
     #num = int((1.0 / dt) * 30)
     #trajectory = dynamics.integrate(x0, 0, 30)
@@ -88,18 +95,50 @@ def main():
     P_initial = jnp.diag(jnp.array([1e-2, 1e-5, 1e-5]))
 
     Q = jnp.diag(jnp.array([1.0, 1.0, 0.001]))
+    R = jnp.diag(jnp.array([1.0, 10.0]))
 
-    ekf = Ekf(X_initial, P_initial, Q, dynamics)
+    U_initial = jnp.array([mt_interpolator.thrust(0.0), mt_interpolator.mass(0.0)])
 
-    altitude_est = []
-    velocity_est = []
-    acceleration_est = []
+    filter = ekf.Ekf(dynamics.f, Q, X_initial, U_initial, P_initial, 3, 2)
 
-    external_force = []
+    def calculate_apogee(X_predict, t):
+        x0 = X_predict
+        x_max = dynamics.integrate(x0, t, 13, dt=0.1)
+        return x_max
 
-    apogee_estimates = []
+    def apogee_default(X_predict, t):
+        return 700.0
 
+    def loop(carry, x):
+        previous_t, filter = carry
+        t, Z, vel = x
+        dt = t - previous_t
+        U = jnp.array([mt_interpolator.thrust(t), mt_interpolator.mass(t)])
 
+        filter = ekf.update_nonlinear(ekf=filter, z=Z, h=dynamics.h, R=R * jnp.where(dt > 0.0, dt, 0.01), U=U, dt=dt, stable=True)
+        X_predict = filter.X
+
+        apogee_estimate = jax.lax.cond(jnp.logical_and(t > 10.0, t < 13.0), calculate_apogee, apogee_default, X_predict, t)
+
+        return (t, filter), (t, X_predict, Z, U, dynamics.a(X_predict, U), apogee_estimate, vel)
+
+    _, series_values = jax.lax.scan(f=loop, init=(0.0, filter), xs=(data_interpolator.times, jnp.stack([data_interpolator.altitude_curve, data_interpolator.acceleration_curve], axis=1), data_interpolator.velocity_curve))
+    time = series_values[0]
+    X = series_values[1]
+    Z = series_values[2]
+    thrust = series_values[3][:, 0]
+    mass = series_values[3][:, 1]
+
+    altitude = Z[:, 0]
+    velocity = series_values[6] # todo: fix this
+    acceleration = Z[:, 1]
+    altitude_est = X[:, 0]
+    velocity_est = X[:, 1]
+    acceleration_est = series_values[4]
+    external_force = X[:, 2]
+    apogee_estimates = series_values[5]
+
+    '''
     for _ in range(int(13 * (1 / dt))):
         print(t)
         ekf.predict(dt)
@@ -136,7 +175,7 @@ def main():
 
         time.append(t)
 
-        t += dt
+        t += dt'''
 
     plot(time, thrust, mass, altitude, velocity, acceleration, altitude_est, velocity_est, acceleration_est, external_force, apogee_estimates)
 
